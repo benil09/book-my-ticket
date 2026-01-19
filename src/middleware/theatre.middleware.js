cc38bce424e45f967dafd6edb9830256ddbc78cb
benil09 <benil8235@gmail.com>
2026-01-19 20:22:22 +0530

    Feature: added middleware validation for the theatre model

---
+++ src/middleware/theatre.middleware.js
@@ -0,0 +1,38 @@
+const errorBody = {
+  success: false,
+  err: "",
+  data: {},
+  message: "malformed request | bad request",
+};
+
+const validateTheatreCreateRequest = (req, res, next) => {
+  // validate theatre name
+  if (!req.body.name) {
+    errorBody.err = "Theatre name is required and should be at least 5 characters long";
+    return res.status(400).json(errorBody);
+  }
+  //validate name length
+//   if (req.body.name.length < 5) {
+//     errorBody.err = "Theatre name must be of atleast 5 characters ";
+//     return res.status(400).json(errorBody);
+//   }
+
+  // validate PIN
+  if (!req.body.PIN) {
+    errorBody.err = "PIN is required";
+    return res.status(400).json(errorBody);
+  }
+
+  //validate city
+  if (!req.body.city) {
+    errorBody.err = "City is required";
+    return res.status(400).json(errorBody);
+  }
+
+
+
+  // if all validations are passed
+  next();
+};
+
+export default { validateTheatreCreateRequest };
--- src/routes/theatres.route.js
+++ src/routes/theatres.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { create } from "../controllers/theatre.controller.js";
+import theatreMiddleware from "../middleware/theatre.middleware.js";
 
 const router = express.Router();
 
-// Create a new 
-router.post("/theatre",create)
+// Create a new theatre
+router.post("/theatre", theatreMiddleware.validateTheatreCreateRequest, create);
 //router.get("/theatre",getTheatres);
 
 
